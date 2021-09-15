65936e9023e33d0f1cbd772dac0ca09a1250cb39
marsxinwang(王鑫) <marsxinwang@tencent.com>
2021-09-15 17:43:03 +0800

    添加更加复杂的demo效果

--- UIViewControllerFrameDemo/KDTwoViewController.m
+++ UIViewControllerFrameDemo/KDTwoViewController.m
@@ -22,26 +22,40 @@
     // tabbarController修改之后，会影响其他的控制器，导致其他view的显示出现问题：在导航栏top开始，在tabbar的top截止
     self.tabBarController.tabBar.translucent = NO;
     // 获取的frame有问题
-    NSLog(@"KDBaseViewController.frame viewWillAppear = %@", [NSValue valueWithCGRect:self.view.frame]);
+    // NSLog(@"KDBaseViewController.frame viewWillAppear = %@", [NSValue valueWithCGRect:self.view.frame]);
     // defalut YES 不透明的
     // self.navigationController.navigationBar.opaque = NO;
+    NSLog(@"%@:viewWillAppear, translucent = NO", self);
 }
 
+// 在此可以拿到正确的self.view.frame
 - (void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
-    NSLog(@"KDBaseViewController.frame viewDidAppear = %@", [NSValue valueWithCGRect:self.view.frame]);
+    // NSLog(@"KDBaseViewController.frame viewDidAppear = %@", [NSValue valueWithCGRect:self.view.frame]);
+    // 理论上讲，tabbar.translucent一开始就会确定，不会来回修改
+    // 目前Demo只是为了测试特定效果，所以在TableViewController & TwoViewController中是不透明的，其他tab是透明的
+    // 因为viewWillAppear顺序导致有问题，所以需要在viewDidAppear重新设置tabbar的透明属性，然后使用self.navigationController去更新self.view.frame，否则难以更新
+    self.tabBarController.tabBar.translucent = NO;
+    [self.navigationController.view setNeedsLayout];
+    [self.navigationController.view layoutSubviews];
 }
 
-- (void)viewDidDisappear:(BOOL)animated {
-    [super viewDidDisappear:animated];
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
     // 还原之后，保证tabbar其他子控制器的view在tabbar的底部截止
     self.tabBarController.tabBar.translucent = YES;
+    NSLog(@"%@:viewWillDisappear, translucent = YES", self);
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    NSLog(@"KDBaseViewController.frame viewDidLoad = %@", [NSValue valueWithCGRect:self.view.frame]);
     self.edgesForExtendedLayout = UIRectEdgeAll;
+}
+
+// 对subview布局，就应该在这类方法中才可以拿到正确的self.view.frame
+- (void)viewDidLayoutSubviews {
+    [super viewDidLayoutSubviews];
+    // NSLog(@"KDTwoViewController.frame viewDidLayoutSubviews = %@", [NSValue valueWithCGRect:self.view.frame]);
 }
 
 @end
